725b43c800a5fae53224852f002b2217e9139d1b
Abdoul_djabarou <babdouldjabar@yahoo.com>
2021-04-15 13:40:16 +0000

    Initialisation du projet avec la mise en place du home page

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div style={{ textAlign:"center" , marginBottom:10}}>
+            Made with love by Abdouldjabar
+            <a href="#" style={{cursor:'pointer'}}>Abdoul</a>
+        </div>
+    )
+}
+
+export default Footer
